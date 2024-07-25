2df7afe6880ffde544bd1ae9a57dfb3b5285e133
gk <gk@adamant.local>
2024-07-25 21:24:15 +0300

    typo fixed. H2

--- Hibernate/src/main/java/org/itstep/Application.java
+++ Hibernate/src/main/java/org/itstep/Application.java
@@ -1,10 +1,14 @@
 package org.itstep;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,12 +22,26 @@ public class Application {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(CountryService countryService) {
+    CommandLineRunner commandLineRunner(CountryService countryService) throws IOException {
         Country country1 = new Country(1L, "Belarus", "BY");
-        List<Country> countries = new ArrayList<>();
-        countries.add(country1);
+//        List<Country> countries = new ArrayList<>();
+//        countries.add(country1);
+//        countryService.saveAll(countries);
+//
+//        };
+        // read json and write to db
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Country> countries = mapper.readValue(Paths.get("Hibernate/src/main/resources/static/countries.json").toFile(),
+                new TypeReference<List<Country>>() {
+                });
+
+
+//countries.stream().forEach(System.out::println);
         countryService.saveAll(countries);
+        System.out.println("Countries saved!");
         return args -> {
+
         };
+        }
     }
-} 
--- Hibernate/src/main/java/org/itstep/CountryControllerFree.java
+++ Hibernate/src/main/java/org/itstep/CountryControllerFree.java
@@ -1,45 +1,49 @@
 package org.itstep;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/api")
-public class CountryController {
+@Controller
+public class CountryControllerFree {
     @Autowired
     CountryService countryService;
 
     @GetMapping("/countries/all")
-    public List<Country> getCountries() {
+    public ModelAndView getCountries() {
         var countries = countryService.findAll();
-        return countries;
+        var params = new HashMap<String, Object>();
+        params.put("countries", countries);
+        return new ModelAndView("showCountries", params);
     }
 
     @GetMapping("/countries/a")
-    public List<Country> getCountriesA() {
+    public ModelAndView getCountriesA() {
         var countries = countryService
                 .findAll()
                 .stream()
                 .filter(country -> country.getName().charAt(0) == 'A')
                 .collect(Collectors.toList());
-        return countries;
+        var params = new HashMap<String, Object>();
+        return new ModelAndView("showCountries", params);
     }
+
 
     @GetMapping("/countries/page")
     public List<Country> getCountriesPage(@RequestParam(defaultValue = "0", name = "page") int page,
                                           @RequestParam(defaultValue = "10", name = "size") int size) {
-        var countries = countryService
-                .findAll(page, size);
+        var countries = countryService.findAll(page, size);
 
         return countries;
     }
+
+
 }
